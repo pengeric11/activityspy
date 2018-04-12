72761a60860226fb1011e8941fa332aeaa6abe9d
Eric Peng <ericpeng@uw.edu>
2018-04-11 17:56:31 -0700

    4/11 Update

--- app/src/main/java/edu/washington/ericpeng/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/ericpeng/activityspy/MainActivity.kt
@@ -2,11 +2,18 @@ package edu.washington.ericpeng.activityspy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("MainActivity", "onCreate event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("MainActivity", "We’re going down, Captain!");
     }
 }
